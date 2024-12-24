fabae15316a49d16a46ff3725f7ae6a770a21d96
Nguyen Truong An <nguyentruongan0610@gmail.com>
2024-12-24 21:17:48 +0700

    test RestApi ( successful)

---
+++ Admin_BackEnd/Movies_Web/src/main/java/com/example/demo/controllers/RestApiController.java
@@ -0,0 +1,78 @@
+/*
+ * @ (#) RestController.java 1.0 12/24/2024
+ *
+ * Copyright (c) 2024 IUH.All rights reserved
+ */
+
+package com.example.demo.controllers;
+
+
+import com.example.demo.models.Category;
+import com.example.demo.models.Movie;
+import com.example.demo.services.CategoryService;
+import com.example.demo.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+/*
+ * @description
+ * @author : Nguyen Truong An
+ * @date : 12/24/2024
+ * @version 1.0
+ */
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+    @Autowired
+    private MovieService movieService;
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<Movie>> movies() {
+        try {
+            List<Movie> movies = movieService.getAllMovies();
+            if (movies.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(movies, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/movies/{id}")
+    public ResponseEntity<Movie> movieById(@PathVariable Long id) {
+        try {
+            Movie movie = movieService.getMovieById(id);
+            if (movie == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(movie, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> categories() {
+        try {
+            List<Category> categories = categoryService.getAllCategory();
+            if (categories.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(categories, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
